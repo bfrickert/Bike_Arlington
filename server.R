11ba642f4eb2abd34e498a2b8079185f5705c699
Brian Frickert <bfrickert@yahoo.com>
2015-09-21 20:09:27 -0400

    Initial Commit

---
+++ server.R
@@ -0,0 +1,51 @@
+library(shiny)
+library(XML)
+library(plyr)
+library(dplyr)
+library(shiny)
+library(forecast)
+
+shinyServer(function(input, output) {
+  getData <- reactive({
+    url <- paste('http://webservices.commuterpage.com/counters.cfc?wsdl&method=GetCountInDateRange&counterid=', input$sensor, '&startDate=01/01/2005&endDate=10/31/2015&direction=&mode=B&interval=d',sep='')
+    doc <- xmlTreeParse(url, useInternal=T)
+    top <- xmlRoot(doc)
+    df <- data.frame(matrix(unlist(xmlApply(top, xmlAttrs)), ncol=4, byrow=T),stringsAsFactors = F)
+    names(df) <- c('count','date','direction', 'type')
+    t <- ts(as.integer(df$count), start=c(2012, 11), end=c(2015, 9), frequency=12)
+    return(t)
+  })
+  getTrain <- reactive({
+    train <- window(getData(),end=c(2015,1))
+    return(train)
+  })
+  getTest <- reactive({
+    test <- window(getData(),start=c(2015,1))
+return(test)
+    })
+  getFit <- reactive({  
+      fit <- stl(getTrain(), t.window=12, s.window="periodic", robust=TRUE)
+    return(fit)
+  })
+  output$otherPlot <- renderPlot({
+      plot(getFit())
+  })
+  output$forecastPlot <- renderPlot({
+    fcast <- forecast(getFit(), method="arima")
+    plot(fcast, ylab="New orders index")
+    lines(getTest(), col='red')
+  })
+ output$accuracy <- renderTable({
+   fcast <- forecast(getFit(), method="arima")
+   forecast::accuracy(fcast, getTest())
+   })
+ output$mymap <- renderLeaflet({
+   df <- read.table('coordinates.tsv',sep='\t',stringsAsFactors = F)
+   names(df) <- c('name', 'lat','lon','id')
+   f <- filter(df, id == input$sensor)
+   m <- leaflet()
+   m <- addTiles(m)
+   m <- addMarkers(m, lng=f$lon, lat=f$lat, popup=f$name)
+   m
+ })
+})
---
+++ ui.R
@@ -0,0 +1,77 @@
+library(shiny)
+library(leaflet)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("You know you want to Forecast Bicycle Usage in Arlington, VA!"),
+  
+  # Sidebar with a slider input for the number of bins
+  sidebarLayout(
+    sidebarPanel(
+      tags$img(src="https://pbs.twimg.com/profile_images/463339101895159808/7bVLf-U2_400x400.jpeg",
+               width="200px",height="200px"),
+      radioButtons("sensor", "Sensor:",
+                   c("Ballston Connector"="24",
+                     "Bluemont Connector"="23",
+                     "CC Connector"="10",
+                     "Custis Bon Air Park" = "3",
+                     "Custis Rosslyn" = "4",
+                     "Clarendon EB bike lane"="20",
+                     "Crystal NB bike lane"="18",
+                     "Crystal SB bike lane"="17",
+                     "Fairfax EB bike lane"="14",
+                     "Fairfax WB bike lane"="13",
+                     "Four Mile Run (piezo)"="5",
+                     "Joyce St NB"="27",
+                     "Joyce St SB"="26",
+                     "Key Bridge East" ="8",
+                     "Key Bridge West" = "7",
+                     "Military NB bike lane" = "22",
+                     "Military SB bike lane" = "21",
+                     "MVT Airport South"="9",
+                     "Quincy NB bike lane"="16",
+                     "Quincy SB bike lane"="15",
+                     "Rosslyn Bikeometer" = "28",
+                     "TR Island Bridge"="11",
+                     "W&OD Bon Air Park"="2",
+                     "W&OD Bon Air West"="25",
+                     "W&OD Columbia Pike"="12",
+                     "W&OD East Falls Church"="1",
+                     "Wilson WB bike lane"="19"
+                     ))
+    ),
+    mainPanel(
+      tags$h3("Synopsis"),
+      tags$h5("The graphs and table below attempt to forecast future bike trail usage in 
+              Arlington County, VA 
+              by leveraging a STL (Seasonal and Trend decomposition using Loess) model 
+              trained on data collected from sensors installed along Arlington's bike trails. 
+              This training set is comprised of sensor readings 
+              from late 2012 to the end of 2014. 
+              The model is then evaluated against data for
+              bike trail usage so far in 2015. That forecast is then plotted
+              along with a decomposition of the training time series. At the very bottom, 
+              the accuracy of each trail's forecast is reported."),
+      tags$br(),
+      tags$h5("So go ahead and pick your favorite trail!"),
+      tags$br(),
+      tags$h5("Data from:"),
+      tags$a(href='http://www.bikearlington.com/pages/biking-in-arlington/counting-bikes-to-plan-for-bikes/data-for-developers/',
+             "Data for Developers"),
+      tags$br(),
+      tags$hr(),
+      tags$h3("Location of Sensor"),
+      leafletOutput("mymap"),
+      tags$br(),
+      tags$h3("Forecast with Cross-Validation"),
+      tags$h5(""),
+      plotOutput("forecastPlot"),
+      tags$h3("Trends"),
+      plotOutput("otherPlot"),
+      tags$h3("Accuracy"),
+      tableOutput("accuracy")
+    )
+  )
+))
